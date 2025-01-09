d037a64676b83cfa493791e3024bd738b5ed35ea
Alessandro-Bk <alessandrojunior889@gmail.com>
2025-01-09 20:32:41 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home () {
-    return <h1> Alexandra, eu amo você, se você me ama de uma risadinha1 </h1>
+    return <h1> Alexandra, eu amo você. se você me ama de uma risadinha! </h1>
 }
 
 export default home;
